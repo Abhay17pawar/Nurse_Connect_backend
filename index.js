03e871bad24cbee8d2e0136a7bf221b153a2637e
Abhay Pawar <abhaypawar0817@gmail.com>
2024-10-22 12:44:13 +0530

    Initial commit for backend

--- index.js
+++ index.js
@@ -2,13 +2,24 @@ const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 require('dotenv').config();
+const cors = require('cors');
+app.use(cors()); 
+app.use(express.json());
 
 const PORT = process.env.PORT;
 const MONGO_URI = process.env.MONGO_URI
 
-mongoose.connect('mongodb+srv://abhaypawar592:KgblgeXNHW2nm1S7@cluster0.5elwj.mongodb.net')
+app.post('/api/emergency', (req, res) => {
+    const { latitude, longitude } = req.body;
+    console.log(`Emergency at location: Latitude: ${latitude}, Longitude: ${longitude}`);
+    
+    res.status(200).send('Location received');
+  });
+
+mongoose.connect(MONGO_URI)
    .then(() => {console.log("mongoDB server is connected!")})
    .catch((error) => {console.error("error connecting to mongoDB")})
+
 
 app.listen(PORT, console.log("server is listening on port 3000..."))
    
